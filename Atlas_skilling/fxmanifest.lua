1cd9d08b19754d0d05dd6415bf81a381c237350d
DrewPamment <drew.pamment@students.mq.edu.au>
2026-04-18 03:00:41 +1000

    DJ KHALED

--- Atlas_skilling/fxmanifest.lua
+++ Atlas_skilling/fxmanifest.lua
@@ -1,21 +1,23 @@
-fx_version 'adamant' -- Changed to adamant (common stable standard for RDR3)
+fx_version 'cerulean'
 game 'rdr3'
-lua54 'yes'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-description 'Atlas Skilling System'
+lua54 'yes'
+
 author 'DrewPamment3'
+description 'Atlas Skilling System - Core XP Manager'
 
--- In modern manifests, we list dependencies like this:
-dependency 'vorp_core'
-dependency 'oxmysql'
-
-shared_script 'shared/config.lua'
+shared_scripts {
+    'shared/config.lua'
+}
 
 client_script 'client/main.lua'
-
 server_script 'server/main.lua'
 
--- Use exports instead of server_export for better compatibility
-exports {
-    'AddSkillXP'
+dependencies {
+    'vorp_core',
+    'oxmysql'
 }
+
+server_export 'AddSkillXP'
+
+version '1.0'
