957734730d035923e2b98e3743fcc8a9602be673
DrewPamment <drew.pamment@students.mq.edu.au>
2026-04-18 02:32:51 +1000

    iaggreee

--- Atlas_skilling/fxmanifest.lua
+++ Atlas_skilling/fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'cerulean'
 game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 description 'Atlas Skilling System - Core XP Manager'
 author 'DrewPamment3'
