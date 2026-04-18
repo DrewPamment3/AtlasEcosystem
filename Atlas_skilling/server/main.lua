95834a5c4923a132c4e66fbb11441d8f04240529
DrewPamment <drew.pamment@students.mq.edu.au>
2026-04-18 14:44:38 +1000

    admin command to give xp

--- Atlas_skilling/server/main.lua
+++ Atlas_skilling/server/main.lua
@@ -74,3 +74,39 @@ function CheckForLevelUp(source, oldXP, newXP)
         print("^3[Atlas Skilling]^7 Player " .. source .. " has leveled up to " .. newLevel)
     end
 end
+
+-- 4. ADMIN COMMAND: Grant XP
+-- Usage: /givexp [ID] [SKILL] [AMOUNT]
+-- Example: /givexp 1 woodcutting 500
+RegisterCommand('givexp', function(source, args)
+    local _source = source
+    local User = VORPcore.getUser(_source)
+    local group = User.group -- Get player group (admin, superadmin, etc)
+
+    -- 1. Permission Check
+    if group == 'admin' or group == 'superadmin' then
+        local targetID = tonumber(args[1])
+        local skillName = tostring(args[2]):lower()
+        local amount = tonumber(args[3])
+
+        -- 2. Validation
+        if targetID and skillName and amount then
+            local Target = VORPcore.getUser(targetID)
+
+            if Target then
+                -- 3. Execute XP Gain
+                -- We call our own export internally
+                exports.atlas_skilling:AddSkillXP(targetID, skillName, amount)
+
+                print('^2[Atlas Admin]^7 Granted ' .. amount .. ' XP in ' .. skillName .. ' to ID: ' .. targetID)
+                VORPcore.NotifyRightTip(_source, "Granted " .. amount .. " XP to ID " .. targetID, 4000)
+            else
+                VORPcore.NotifyRightTip(_source, "Player ID not found.", 4000)
+            end
+        else
+            VORPcore.NotifyRightTip(_source, "Usage: /givexp [id] [skill] [amount]", 4000)
+        end
+    else
+        VORPcore.NotifyRightTip(_source, "You do not have permission to use this.", 4000)
+    end
+end, false)
