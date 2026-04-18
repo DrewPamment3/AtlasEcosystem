d147949ea8673392220e3f5ac968ad6a1c6e4842
DrewPamment <drew.pamment@students.mq.edu.au>
2026-04-19 01:49:50 +1000

    s

--- Atlas_woodcutting/client/main.lua
+++ Atlas_woodcutting/client/main.lua
@@ -1,24 +1,42 @@
 local isBusy = false
-local LocalTreeEntities = {}
+local LocalTrees = {} -- [entityHandle] = true
 
--- [[ NODE SYNC & SPAWNING ]]
+local function DrawTxt(text, x, y)
+    SetTextScale(0.35, 0.35)
+    SetTextColor(255, 255, 255, 255)
+    SetTextCentre(true)
+    local str = CreateVarString(10, "LITERAL_STRING", text)
+    DisplayText(str, x, y)
+end
+
+-- [[ SPAWNING ]]
 
 local function SpawnLocalTree(node)
     local model = node.model_hash
     if not HasModelLoaded(model) then
         RequestModel(model)
-        while not HasModelLoaded(model) do Citizen.Wait(1) end
+        local timeout = 0
+        while not HasModelLoaded(model) and timeout < 100 do
+            Citizen.Wait(10)
+            timeout = timeout + 1
+        end
     end
 
-    local tree = CreateObject(model, node.x, node.y, node.z, false, false, false)
-    FreezeEntityPosition(tree, true)
-    SetEntityAsMissionEntity(tree, true, true)
-    table.insert(LocalTreeEntities, tree)
-    SetModelAsNoLongerNeeded(model)
+    if HasModelLoaded(model) then
+        local tree = CreateObject(model, node.x, node.y, node.z, false, false, false)
+        FreezeEntityPosition(tree, true)
+        SetEntityAsMissionEntity(tree, true, true)
+        LocalTrees[tree] = true
+        SetModelAsNoLongerNeeded(model)
+    end
 end
 
 RegisterNetEvent('Atlas_Woodcutting:Client:SyncNodes')
 AddEventHandler('Atlas_Woodcutting:Client:SyncNodes', function(nodes)
+    for entity, _ in pairs(LocalTrees) do
+        if DoesEntityExist(entity) then DeleteEntity(entity) end
+    end
+    LocalTrees = {}
     for _, node in ipairs(nodes) do SpawnLocalTree(node) end
 end)
 
@@ -27,8 +45,13 @@ AddEventHandler('Atlas_Woodcutting:Client:SpawnSingleNode', function(node)
     SpawnLocalTree(node)
 end)
 
--- Run this on startup/join
+-- Init Sync
 Citizen.CreateThread(function()
+    Citizen.Wait(4000) -- Delay to let server DB queries finish
+    TriggerServerEvent('Atlas_Woodcutting:Server:PlayerLoaded')
+end)
+
+RegisterCommand('refresh_trees', function()
     TriggerServerEvent('Atlas_Woodcutting:Server:PlayerLoaded')
 end)
 
@@ -37,13 +60,16 @@ end)
 RegisterNetEvent('Atlas_Woodcutting:Client:GenerateForestNodes')
 AddEventHandler('Atlas_Woodcutting:Client:GenerateForestNodes', function(forestId, center, radius, count, modelName)
     for i = 1, count do
-        local angle, r = math.random() * 2 * math.pi, radius * math.sqrt(math.random())
-        local x, y = center.x + r * math.cos(angle), center.y + r * math.sin(angle)
+        local angle = math.random() * 2 * math.pi
+        local r = radius * math.sqrt(math.random())
+        local x = center.x + r * math.cos(angle)
+        local y = center.y + r * math.sin(angle)
         local foundGround, groundZ = GetGroundZFor_3dCoord(x, y, 1000.0, 0)
+
         if foundGround then
             TriggerServerEvent('Atlas_Woodcutting:Server:SaveNode', forestId, vec3(x, y, groundZ), modelName)
         end
-        Citizen.Wait(200)
+        Citizen.Wait(300)
     end
 end)
 
@@ -52,27 +78,20 @@ end)
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        local pCoords = GetEntityCoords(PlayerPedId())
-        local pForward = GetEntityForwardVector(PlayerPedId())
-        local start, target = pCoords + vec3(0, 0, 0.6), pCoords + (pForward * 2.2) + vec3(0, 0, 0.6)
+        local playerPed = PlayerPedId()
+        local pCoords = GetEntityCoords(playerPed)
+        local pForward = GetEntityForwardVector(playerPed)
 
-        local ray = StartShapeTestRay(start.x, start.y, start.z, target.x, target.y, target.z, 273, PlayerPedId(), 0)
+        local start = pCoords + vec3(0, 0, 1.0)
+        local target = pCoords + (pForward * 2.0) + vec3(0, 0, 1.0)
+        local ray = StartShapeTestRay(start.x, start.y, start.z, target.x, target.y, target.z, 16, playerPed, 0)
         local _, hit, _, _, entityHit, _ = GetShapeTestResult(ray)
 
-        -- FIX: Use Type check to avoid Native Crash
-        if hit == 1 and entityHit ~= 0 and GetEntityType(entityHit) == 3 then
-            local treeCoords = GetEntityCoords(entityHit)
-            local model = GetEntityModel(entityHit)
-
-            -- If it's one of our props or a known tree
-            if model == `p_tree_pine_01` or Config.Trees[model] then
-                local str = CreateVarString(10, "LITERAL_STRING", "Tree Detected | [G] Chop")
-                SetTextScale(0.35, 0.35)
-                DisplayText(str, 0.5, 0.88)
-
-                if IsControlJustReleased(0, 0x760A9C6F) and not isBusy then
-                    TriggerServerEvent('Atlas_Woodcutting:Server:RequestStart', treeCoords)
-                end
+        -- Strict lookup to prevent crashing on map objects
+        if hit == 1 and LocalTrees[entityHit] then
+            DrawTxt("Harvest Tree [G]", 0.5, 0.88)
+            if IsControlJustReleased(0, 0x760A9C6F) and not isBusy then
+                TriggerServerEvent('Atlas_Woodcutting:Server:RequestStart', GetEntityCoords(entityHit))
             end
         end
     end
