76e3a17f3207ba5547ef8809d28481a5b4943905
DrewPamment <drew.pamment@students.mq.edu.au>
2026-04-18 22:00:23 +1000

    manifest

---
+++ Atlas_woodcutting/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+lua54 'yes'
+
+author 'DrewPamment3'
+description 'Atlas Woodcutting - RPG Gathering Module'
+version '1.0'
+
+shared_scripts {
+    'shared/config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+dependencies {
+    'vorp_core',
+    'vorp_inventory',
+    'oxmysql',
+    'Atlas_Skilling' -- Essential: Allows use of AddSkillXP and GetSkillLevel
+}
