8ec156fa153f27d186748071ac9348efdbc77ff9
DrewPamment <drew.pamment@students.mq.edu.au>
2026-04-19 13:58:05 +1000

    Update main.lua

--- Atlas_woodcutting/client/main.lua
+++ Atlas_woodcutting/client/main.lua
@@ -1,32 +1,43 @@
 local isBusy = false
 local LocalTrees = {} -- [entityHandle] = forestId
 
-local function DrawTxt(text, x, y)
+-- [[ UI DRAWING ]]
+
+local function DrawWoodcuttingPrompt()
+    -- Background Box for the Button
+    DrawRect(0.5, 0.91, 0.15, 0.045, 0, 0, 0, 150)
+
+    -- The "G" Button Look
+    SetTextScale(0.38, 0.38)
+    SetTextColor(0, 0, 0, 255) -- Black text for the button
+    SetTextCentre(true)
+    local gButton = CreateVarString(10, "LITERAL_STRING", "G")
+
+    -- Draw a white "button" circle/square behind the G
+    DrawRect(0.465, 0.91, 0.02, 0.03, 255, 255, 255, 255)
+    DisplayText(gButton, 0.465, 0.895)
+
+    -- The Prompt Text
     SetTextScale(0.35, 0.35)
     SetTextColor(255, 255, 255, 255)
-    SetTextCentre(true)
-    local str = CreateVarString(10, "LITERAL_STRING", text)
-    DisplayText(str, x, y)
+    SetTextCentre(false)
+    SetTextFontForCurrentCommand(1) -- High-quality RDR3 serif font
+    local promptText = CreateVarString(10, "LITERAL_STRING", "CUT TREE")
+    DisplayText(promptText, 0.485, 0.895)
 end
 
 -- [[ SPAWNING LOGIC ]]
 
 local function SpawnLocalTree(node)
     local modelHash = GetHashKey(node.model_name)
-
     if not HasModelLoaded(modelHash) then
         RequestModel(modelHash)
         while not HasModelLoaded(modelHash) do Citizen.Wait(1) end
     end
 
     local _, groundZ = GetGroundZFor_3dCoord(node.x, node.y, 1000.0, 0)
+    local tree = CreateObject(modelHash, node.x, node.y, groundZ - 0.2, false, false, false)
 
-    -- Burial: Sinks roots by 0.2m to prevent hover
-    local buriedZ = groundZ - 0.2
-
-    local tree = CreateObject(modelHash, node.x, node.y, buriedZ, false, false, false)
-
-    -- Vertical Growth + Random Yaw
     local randomYaw = math.random(0, 360) + 0.0
     SetEntityRotation(tree, 0.0, 0.0, randomYaw, 2, true)
 
@@ -38,7 +49,6 @@ end
 
 -- [[ SYNC & CLEANUP ]]
 
--- Use RegisterNetEvent on the client
 RegisterNetEvent('Atlas_Woodcutting:Client:SyncNodes')
 AddEventHandler('Atlas_Woodcutting:Client:SyncNodes', function(nodes)
     for entity, _ in pairs(LocalTrees) do
@@ -76,7 +86,6 @@ AddEventHandler('Atlas_Woodcutting:Client:GenerateForestNodes', function(forestI
         local angle, r = math.random() * 2 * math.pi, radius * math.sqrt(math.random())
         local x, y = center.x + r * math.cos(angle), center.y + r * math.sin(angle)
         local foundGround, groundZ = GetGroundZFor_3dCoord(x, y, 1000.0, 0)
-
         if foundGround then
             TriggerServerEvent('Atlas_Woodcutting:Server:SaveNode', forestId, vec3(x, y, groundZ), modelName)
         end
@@ -84,19 +93,24 @@ AddEventHandler('Atlas_Woodcutting:Client:GenerateForestNodes', function(forestI
     end
 end)
 
--- [[ INTERACTION ]]
+-- [[ INTERACTION LOOP ]]
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        local pCoords = GetEntityCoords(PlayerPedId())
-        local pForward = GetEntityForwardVector(PlayerPedId())
+        local playerPed = PlayerPedId()
+        local pCoords = GetEntityCoords(playerPed)
+        local pForward = GetEntityForwardVector(playerPed)
+
+        -- Detect the entity in front
         local start, target = pCoords + vec3(0, 0, 1.0), pCoords + (pForward * 2.2) + vec3(0, 0, 1.0)
-        local ray = StartShapeTestRay(start.x, start.y, start.z, target.x, target.y, target.z, 16, PlayerPedId(), 0)
+        local ray = StartShapeTestRay(start.x, start.y, start.z, target.x, target.y, target.z, 16, playerPed, 0)
         local _, hit, _, _, entityHit, _ = GetShapeTestResult(ray)
 
+        -- Validation: Is it one of OUR spawned trees?
         if hit == 1 and LocalTrees[entityHit] then
-            DrawTxt("Harvest Tree [G]", 0.5, 0.88)
+            DrawWoodcuttingPrompt()
+
             if IsControlJustReleased(0, 0x760A9C6F) and not isBusy then
                 TriggerServerEvent('Atlas_Woodcutting:Server:RequestStart', GetEntityCoords(entityHit))
             end
